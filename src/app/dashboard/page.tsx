c272ea777edb6a370c0a6d351756265a4a99caac
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-10-03 04:13:11 +0500

    123 added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello from dashboard123
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { redirect } from 'next/navigation'
+
+const page = () => {
+  redirect("/dashboard")
+
+}
+
+export default page
